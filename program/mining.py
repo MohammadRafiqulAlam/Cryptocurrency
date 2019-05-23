d597fe0b66e9ecea16ae81e37847440c9d7a8f47
Mohammad Alam <mohammad.alam@cbh.com.au>
2019-05-23 12:59:24 +0800

    Update code / user guide

--- program/mining.py
+++ program/mining.py
@@ -1,33 +1,52 @@
 import time
 import hashlib
 
+# function that calculates the nonce
 def calculate_nonce(block_index, previous_hash, new_transaction):
-    print(block_index,previous_hash,new_transaction)
+    #print(block_index,previous_hash,new_transaction)
+    # initialize nonce to 0
     nonce = 0
+    # an empty block
     new_block = []
+    # add data to the block
     new_block.append(str(block_index) + " " + str(time.time()) + \
         " " + new_transaction + " " + previous_hash + " ")
-    print(new_block)    
+    # print(new_block)    
     while nonce<=50001 :
         #print(nonce)
+        # append nonce to the block
         new_block_str = new_block[-1] + " " + str(nonce) 
+        # get the sha256 hexdigest
         sh256 = hashlib.sha256(new_block_str.encode()).hexdigest()
+        # convert sha256 into binary format 
         bin_sh256 = bin(int(sh256,16))[2:]
+        # check if there is 14 zeros at the start or nonce is 50001
         if len(bin_sh256.split('1',1)[0]) == 14 or nonce == 50001:
             file = open('blockchain.txt','a')
             #file.write('\n')
+            # write to the blockchain file
             file.write('\n' + str(block_index) + " " + str(sh256) + " " + str(nonce))
             file.close()
             print("nonce = ", nonce)
             break
         nonce += 1
 
+# Retreives the latest transaction from the transaction file
 def latest_transaction_index():
     file = open("transactions.txt","r")
-    lineList = file.readlines()
-    file.close
-    return int(lineList[-1].split(" ",1)[0]) + 1
+    firstLine = file.read(1)
+    file.close()
+    if not firstLine:
+        return 0
+    else:  
+        file = open("transactions.txt","r")      
+        lineList = file.readlines()
+        file.close()
+        return int(lineList[-1].split(" ",1)[0]) + 1
+    
+    
 
+# continuously ask for user choice
 def get_choice():
     return input("Do you want to look for a new transaction(Y/N)?: ")
 
@@ -36,38 +55,45 @@ def main():
     file = open("blockchain.txt","r")
     firstLine = file.read(1)
     file.close()
-    #check if the file is empty
+    # get the lates transaction index
     current_tr_index = latest_transaction_index()
+    #check if the file is empty
     if not firstLine:
         file = open("blockchain.txt","a")
         block = []
+        # first block data
         data = "first block"
         block.append(str(0) + data + str(time.time()) + str(hashlib.sha256(data.encode()).hexdigest()))
         file.write(str(0) + " " + str(hashlib.sha256(data.encode()).hexdigest()) )
-        file.close()     
+        file.close()
+    # get user choice         
     choice = get_choice()
     while choice == 'Y':
+        # check if there is any new transaction added to trnsactions.txt file
         if current_tr_index == latest_transaction_index():
             print("No new transaction detected!!")
             choice = get_choice()
         else:
             while current_tr_index < latest_transaction_index():
+                # read the transaction
                 file = open("transactions.txt","r")
                 newLine = file.readlines()[current_tr_index]
-                print(newLine)
+                #print(newLine)
                 file.close()
                 
+                # read from the blockchain file
                 file = open("blockchain.txt","r")
                 lineList_blck = file.readlines()
-                file.close
+                file.close()
+                # get the previous hash
                 prev_hash = lineList_blck[-1].split(" ",1)[1]
+                # generate block index
                 blc_indx = int(lineList_blck[-1].split(" ",1)[0])
 
-                print(prev_hash, blc_indx)
+                # print(prev_hash, blc_indx)
+                # calculate nonce 
                 calculate_nonce(blc_indx+1, prev_hash, newLine)
                 current_tr_index += 1 
-
-
 
 if __name__ == "__main__":
     main()
--- program/transactions.py
+++ program/transactions.py
@@ -1,25 +1,41 @@
 import time 
+
+# function to get transaction input from user
 def transaction_data():
     from_val = input("From: ")
     to_val = input("To: ")
     amount_val = input("Amount: ")
     return from_val, to_val,amount_val  
-#def create_output_file():Y
 
+# the main function 
 def main():
     choice = input("Do you want to add a new transaction(Y/N)?: ")
+    # read from the trnsaction.txt file
     file = open("transactions.txt","r")
-    lineList = file.readlines()
-    file.close
-
-    counter = int(lineList[-1].split(" ",1)[0]) + 1
-    file = open("transactions.txt","a")  
-    while choice == 'Y': 
-        frm, to, amnt = transaction_data()
-        file.write(str(counter) + " From: "+frm+ " To: "+to+ " Amount: "+ amnt) #+ " Timestamp: " + str(time.time()) )
-        file.write("\n") 
-        counter += 1    
-        choice = input("Do you want to add a new transaction(Y/N)?: ")
+    # get how many transactions are recorded?
+    firstLine = file.read(1)
+    if not firstLine:
+        counter = 0
+    else: 
+        lineList = file.readlines()
+        # variable used for tracking number of transactions
+        counter = int(lineList[-1].split(" ",1)[0]) + 1
+    # close the tranaction file 
     file.close()
+    
+    while choice == 'Y':
+        file = open("transactions.txt","a")  
+        # get transaction data  
+        frm, to, amnt = transaction_data()
+        # write to transaction file
+        if counter > 0:
+            file.write("\n") 
+        
+        file.write(str(counter) + " From: "+frm+ " To: "+to+ " Amount: "+ amnt)
+        # increment the counter     
+        counter += 1    
+        file.close()
+        choice = input("Do you want to add a new transaction(Y/N)?: ")
 if __name__ == "__main__":
     main() 
+
